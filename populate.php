ea6cd6e11478af56dc163cb7305397987da1ec79
jsilber94 <jessesilber@gmail.com>
2016-10-04 16:07:28 -0400

    fixing jons mistakes! echo is a keyword

--- populate.php
+++ populate.php
@@ -1,40 +1,40 @@
 <?php
 
-$url = 'https://foodgawker.com';
-$ch = curl_init($url);
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-$page = curl_exec($ch);
-
-curl_close($ch);
-
-$domdocument = new DomDocument();
-@$domdocument->loadHTML($page);
-
-
-$xpath = new DOMXpath(@$domdocument);
-
-$amount = ($xpath->query("//div[@class='flipwrapper']")->length);
-
 include 'utils.php';
 setupTable();
-
 include 'scrape.php';
-for ($i = 0; $i < $amount; $i++) {
-    $title = $xpath->query("//div[@class='flipwrapper']")->item($i)->getAttribute('data-sharetitle');
-    $link = $xpath->query("//div[@class='flipwrapper']")->item($i)->getAttribute('data-shareurl');
-    $description = $xpath->query("//div[@class='flipwrapper']")->item($i)->getAttribute('data-sharecontent');
-    $username = $xpath->query("//a[@class='submitter']")->item($i)->nodeValue;
-    $faved = $xpath->query("//div[@class='faved']")->item($i)->nodeValue;
-    $gawked = $xpath->query("//div[@class='gawked']")->item($i)->nodeValue;
+
+
+$amountOfPages = 101;
+
+for ($j = 1; $j < $amountOfPages; $j++) {
     
-    $entry = new Scrape($title, $link, $description, $username, $faved, $gawked);
-    $entry->addDBEntry();
+    $url = 'https://foodgawker.com/page/'.$j;
     
-    //addEntry($title,$link, $description, $username, $faved, $gawked);
-    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $page = curl_exec($ch);
+    curl_close($ch);
+    $domdocument = new DomDocument();
+    @$domdocument->loadHTML($page);
+    $xpath = new DOMXpath(@$domdocument);
+    $amount = ($xpath->query("//div[@class='flipwrapper']")->length);
+
+    for ($i = 0; $i < $amount; $i++) {
+        $title = $xpath->query("//div[@class='flipwrapper']")->item($i)->getAttribute('data-sharetitle');
+        $link = $xpath->query("//div[@class='flipwrapper']")->item($i)->getAttribute('data-shareurl');
+        $description = $xpath->query("//div[@class='flipwrapper']")->item($i)->getAttribute('data-sharecontent');
+        $username = $xpath->query("//a[@class='submitter']")->item($i)->nodeValue;
+        $faved = $xpath->query("//div[@class='faved']")->item($i)->nodeValue;
+        $gawked = $xpath->query("//div[@class='gawked']")->item($i)->nodeValue;
+
+        $entry = new Scrape($title, $link, $description, $username, $faved, $gawked);
+        $entry->addDBEntry();
+
+        //addEntry($title,$link, $description, $username, $faved, $gawked);
+    }
     sleep(1);
 }
+
 
 ?>
